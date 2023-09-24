c83cb85acec8e41e3bd44b510b8ad9765b3e44ff
DominikSkubisz <120267115+DominikSkubisz@users.noreply.github.com>
2023-09-24 06:37:51 +0200

    Update mkpasswd.py
    the crypt library was replaced because it was deprecated /
    biblioteka crypt została zastąpiona, ponieważ była przestarzała

--- mkpasswd.py
+++ mkpasswd.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-import os
-import sys
-import crypt
 import getpass
+from passlib.hash import sha512_crypt
+
 
 if __name__ == "__main__":
-    pwd = str(getpass.getpass("Password:"))
-    pwd2 = str(getpass.getpass("Retype password:"))
-    if pwd == pwd2:
-        print(crypt.crypt(pwd, crypt.mksalt(crypt.METHOD_SHA512)))
+    pwd = getpass.getpass("Password:")
+    pwd_rep = getpass.getpass("Retype password:")
+    if pwd == pwd_rep:
+        print(sha512_crypt.hash(pwd))
     else:
-        print("Passwords not match")
+        print("Passwords do not match")
