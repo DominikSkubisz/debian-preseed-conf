cfef788f1e9dfaa4eb94b8f87f127a3015d08ce5
DominikSkubisz <120267115+DominikSkubisz@users.noreply.github.com>
2023-10-01 03:00:37 +0200

    Update mkpasswd.py
    refactored the code to be able to import mkhash function /
    zrefaktoryzowano kod aby móc importować funkcje mkhash

--- mkpasswd.py
+++ mkpasswd.py
@@ -3,10 +3,17 @@ import getpass
 from passlib.hash import sha512_crypt
 
 
+def mkhash(password):
+    if password == '':
+        return ''
+    else:
+        return sha512_crypt.hash(password)
+
+
 if __name__ == "__main__":
     pwd = getpass.getpass("Password:")
     pwd_rep = getpass.getpass("Retype password:")
     if pwd == pwd_rep:
-        print(sha512_crypt.hash(pwd))
+        print(mkhash(pwd))
     else:
         print("Passwords do not match")
